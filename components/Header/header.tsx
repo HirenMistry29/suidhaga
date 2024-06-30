ac0df8b503be85c01fced15ed167309fc5e74212
Nikhil882 <nikhilvaishya88@gmail.com>
2024-06-30 17:56:40 +0530

    UI Changes, Active/Inactive status in Jobs

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -8,6 +8,7 @@ import toast from "react-hot-toast";
 import { useNavigate } from '@/hooks/useRoute';
 import { useMutation } from "@apollo/client";
 import { LOGOUT } from "@/graphql/mutations/users.mutations";
+import { usePathname } from "next/navigation";
 
 interface ChildProp {
     userType : String
@@ -19,6 +20,16 @@ interface ChildProp {
     userType : String
 }
 
+function getRoute() { 
+    const path = usePathname()
+
+    if(path.startsWith('/admin')){
+        return true;
+    }
+    else {
+        return false;
+    }
+} 
 
 const Header:React.FC<ChildProp> = ({userType}) => {
 
@@ -35,6 +46,7 @@ const Header:React.FC<ChildProp> = ({userType}) => {
         }
         
     }
+    const isAdmin = getRoute();
 
 
     return(
@@ -42,12 +54,14 @@ const Header:React.FC<ChildProp> = ({userType}) => {
             <div className="fixed top-0 flex flex-row w-screen bg-gradient-to-r from-[#00154F] to-[#1743B9] via-[#172651] px-[3%] h-10 justify-between text-white">
                 <div className="flex flex-row h-[80%] w-[30%] px-1 xl:w-[10%] sm:w-[10%]">
                     <Image src={logo} alt=""/>
-                    <div className="font-bold py-[5px] px-1  ">
+                    <div className="font-bold py-[5px] px-1">
                     Akshaydhaga
                 </div>
                 </div>
                 <div className="flex flex-row xl:gap-4 lg:gap-4 sm:gap-1 gap-1 py-[5px]">
                     { userType==='Admin' && <span className="flex flex-row gap-1 xl:gap-4"> <span onClick={()=>route('/admin')} className="cursor-pointer hover:text-[#C7C7C7] ">Admin</span>
+                    <span>|</span></span> }
+                    { isAdmin && <span className="flex flex-row gap-1 xl:gap-4"> <span onClick={()=>route('/jobs')} className="cursor-pointer hover:text-[#C7C7C7] ">Jobs</span> 
                     <span>|</span></span> }
                     <span onClick={()=>route('/auth/login')} className="cursor-pointer hover:text-[#C7C7C7] ">login</span>
                     <span>|</span>
